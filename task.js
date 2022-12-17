fa5a51f0baf3021d364a0a1a813c2624721af7d3
Igor Khomenko <xomigrek@gmail.com>
2022-12-17 11:07:17 +0300

    Base-concepts

---
+++ task.js
@@ -0,0 +1,17 @@
+"use strict"
+function solveEquation(a, b, c) {
+  let arr = [];
+  let d = Math.pow(b , 2) - (4*a*c);
+  if (d === 0) {
+    arr.push((-b / ( 2 * a )))
+  } {
+    if (d > 0) {
+      arr.push ((-b + Math.sqrt(d) )/(2*a) , (-b - Math.sqrt(d) )/(2*a))
+    }
+  }
+ 
+  return arr;
+}
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+}
